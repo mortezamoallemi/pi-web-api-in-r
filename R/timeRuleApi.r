18e6c78ae75c3b08a6222a9a5351207d4e290abe
Morteza Moallemi <41530553+mortezamoallemi@users.noreply.github.com>
2020-10-07 12:14:35 +1100

    Add files via upload

---
+++ R/timeRuleApi.r
@@ -0,0 +1,107 @@
+timeRuleApi <- R6Class("timeRuleApi",
+	private = list(),
+	public = list(
+		serviceBase = NULL,
+		authType = NULL,
+		username = NULL,
+		password = NULL,
+		validateSSL = NULL,
+		debug = NULL,
+		initialize = function(baseUrl, authType, username, password, validateSSL, debug) {
+			self$serviceBase <- baseUrl
+			self$username <- username
+			self$password <- password
+			self$authType <- authType
+			self$validateSSL <- validateSSL
+			self$debug <- debug
+		},
+		getByPath = function(path, selectedFields, webIdType) {
+			qs <- customQueryString$new()
+			if (is.null(path) || path == "") {
+				return (paste0("Error: required parameter path was null or undefined"))
+			}
+			if (is.character(path) == FALSE) {
+				return (print(paste0("Error: path must be a string.")))
+			}
+			qs$add('path', path, FALSE);
+			localVarPath <- paste(c(self$serviceBase, '/timerules'), collapse = "")
+			if (missing(selectedFields) == FALSE && is.null(selectedFields) == FALSE && selectedFields != "") {
+				qs$add('selectedFields', selectedFields, FALSE);
+				if (is.character(selectedFields) == FALSE) {
+					return (print(paste0("Error: selectedFields must be a string.")))
+				}
+			}
+			if (missing(webIdType) == FALSE && is.null(webIdType) == FALSE && webIdType != "") {
+				qs$add('webIdType', webIdType, FALSE);
+				if (is.character(webIdType) == FALSE) {
+					return (print(paste0("Error: webIdType must be a string.")))
+				}
+			}
+			res <- getHttpRequest(localVarPath, qs$getQueryParameters(), self$username, self$password, self$authType, self$validateSSL, self$debug)
+			contentResponse <- content(res)
+			if (res$status == 200) {
+				attr(contentResponse, "className") <- "PITimeRule"
+			}
+			return (contentResponse)
+		},
+		get = function(webId, selectedFields, webIdType) {
+			qs <- customQueryString$new()
+			if (is.null(webId) || webId == "") {
+				return (paste0("Error: required parameter webId was null or undefined"))
+			}
+			if (is.character(webId) == FALSE) {
+				return (print(paste0("Error: webId must be a string.")))
+			}
+			localVarPath <- paste(c(self$serviceBase, '/timerules/', webId), collapse = "")
+			if (missing(selectedFields) == FALSE && is.null(selectedFields) == FALSE && selectedFields != "") {
+				qs$add('selectedFields', selectedFields, FALSE);
+				if (is.character(selectedFields) == FALSE) {
+					return (print(paste0("Error: selectedFields must be a string.")))
+				}
+			}
+			if (missing(webIdType) == FALSE && is.null(webIdType) == FALSE && webIdType != "") {
+				qs$add('webIdType', webIdType, FALSE);
+				if (is.character(webIdType) == FALSE) {
+					return (print(paste0("Error: webIdType must be a string.")))
+				}
+			}
+			res <- getHttpRequest(localVarPath, qs$getQueryParameters(), self$username, self$password, self$authType, self$validateSSL, self$debug)
+			contentResponse <- content(res)
+			if (res$status == 200) {
+				attr(contentResponse, "className") <- "PITimeRule"
+			}
+			return (contentResponse)
+		},
+		update = function(webId, PITimeRule) {
+			qs <- customQueryString$new()
+			if (is.null(webId) || webId == "") {
+				return (paste0("Error: required parameter webId was null or undefined"))
+			}
+			if (is.character(webId) == FALSE) {
+				return (print(paste0("Error: webId must be a string.")))
+			}
+			if (is.null(PITimeRule) || PITimeRule == "") {
+				return (paste0("Error: required parameter PITimeRule was null or undefined"))
+			}
+			className <- attr(PITimeRule, "className")
+			if ((is.null(className)) || (className != "PITimeRule")) {
+				return (print(paste0("Error: the class from the parameter PITimeRule should be PITimeRule.")))
+			}
+			localVarPath <- paste(c(self$serviceBase, '/timerules/', webId), collapse = "")
+			res <- patchHttpRequest(localVarPath, qs$getQueryParameters(), PITimeRule, self$username, self$password, self$authType, self$validateSSL, self$debug)
+			return (res)
+		},
+		delete = function(webId) {
+			qs <- customQueryString$new()
+			if (is.null(webId) || webId == "") {
+				return (paste0("Error: required parameter webId was null or undefined"))
+			}
+			if (is.character(webId) == FALSE) {
+				return (print(paste0("Error: webId must be a string.")))
+			}
+			localVarPath <- paste(c(self$serviceBase, '/timerules/', webId), collapse = "")
+			res <- deleteHttpRequest(localVarPath, qs$getQueryParameters(), self$username, self$password, self$authType, self$validateSSL, self$debug)
+			return (res)
+		}
+	)
+)
